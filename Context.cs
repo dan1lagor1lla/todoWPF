e9a9b84c973ba7d844ab0df6b1ab78b2cdf1d456
dany <betallongreen@gmail.com>
2024-04-30 13:21:10 +0400

    initial commit

---
+++ Context.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace wpf_todo
+{
+    internal class Context : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=todo;Trusted_Connection=true;");
+        }
+    }
+
+    public class Task
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public bool IsDone { get; set; }
+
+        public Task() { }
+        public Task(string content, bool isDone)
+        {
+            Content = content;
+            IsDone = isDone;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_todo
+{
+    public partial class MainWindow : Window
+    {
+        private Context db = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            db.Tasks.Load();
+
+            Tasks.ItemsSource = db.Tasks.Local.ToObservableCollection();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TaskContent.Text.Trim()))
+                return;
+            db.Tasks.Add(new Task(TaskContent.Text, false));
+            db.SaveChanges();
+            TaskContent.Text = string.Empty;
+        }
+
+        private void TaskContentChanged(object sender, TextChangedEventArgs e) => db.SaveChanges();
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            db.Tasks.Remove((Task)((Button)sender).DataContext);
+            db.SaveChanges();
+        }
+
+        private void IsDoneChanged(object sender, RoutedEventArgs e) => db.SaveChanges();
+    }
+}
